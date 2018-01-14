90424f69ee8e3a33ec92abcf87c0ec674b9ef65e
Kyle Swanson <swansonk.14@gmail.com>
2018-01-15 00:05:50 +0600

    Adding lab 4

---
+++ Labs/Lab4/lab4.py
@@ -0,0 +1,81 @@
+from tqdm import trange
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+
+#-------------------------------------------------------------------------------
+# Part 1 - Kernel Perceptron
+#-------------------------------------------------------------------------------
+
+class KernelPerceptron(BaseEstimator, ClassifierMixin):
+    def __init__(self, kernel=lambda x_j, x_i: np.dot(x_j, x_i), T=5):
+        """Initializes the KernelPerceptron.
+
+        Arguments:
+            kernel(function): The kernel function to use.
+            T(int): The number of iterations to perform in the Perceptron algorithm.
+        """
+
+        self.kernel = kernel
+        self.T = T
+
+    def fit(self, X, y):
+        """Fits (trains) the KernelPerceptron on the provided data and labels.
+
+        X(ndarray): An NxD numpy array where N is the number of data points
+                    and D is the number of features (dimensions) of each data point.
+        y(ndarray): A length N numpy array with the labels for each data point.
+
+        Returns:
+            The trained KernelPerceptron.
+        """
+
+        # Confirm valid input
+        X, y = check_X_y(X, y)
+        self.classes_ = unique_labels(y)
+
+        # Save training data and labels for use in predict function
+        self.X_ = X
+        self.y_ = y
+
+        # Initialize alpha
+        n = X.shape[0]
+
+        self.alpha = np.zeros(n)
+
+        # Kernel perceptron algorithm
+        # YOUR CODE GOES HERE
+
+        # END OF YOUR CODE
+        
+        return self
+
+    def predict(self, X):
+        """Predicts the labels for the provided data.
+
+        Arguments:
+            X(ndarray): An NxD numpy array where N is the number of data points
+                        and D is the number of features (dimensions) of each data point.
+
+        Returns:
+            A length N numpy array containing the predicted labels for each data point.
+        """
+
+        # Confirm valid input
+        check_is_fitted(self, ['X_', 'y_'])
+        X = check_array(X)
+
+        # Initialize predictions array
+        n_ = self.X_.shape[0]
+        n = X.shape[0]
+
+        predictions = np.ones(n)
+
+        # Generate predictions
+        # YOUR CODE GOES HERE
+
+        # END OF YOUR CODE
+
+        return predictions
