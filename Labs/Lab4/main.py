98ad252f300126f6e5220370cf53485300b46e66
Kyle Swanson <swansonk.14@gmail.com>
2018-01-15 11:41:47 +0600

    Commenting main for lab 4

--- Labs/Lab4/main.py
+++ Labs/Lab4/main.py
@@ -38,24 +38,24 @@ plt.show()
 # Part 2 - Linear Models
 #-------------------------------------------------------------------------------
 
-def linear_kernel(x_j, x_i):
-    raise NotImplementedError
+# def linear_kernel(x_j, x_i):
+#     raise NotImplementedError
 
-clf = lab4.KernelPerceptron(kernel=linear_kernel)
-clf.fit(X, y_linear)
+# clf = lab4.KernelPerceptron(kernel=linear_kernel)
+# clf.fit(X, y_linear)
 
-ax1 = plt.subplot(121)
-utils.plot_classifier(ax1, X, y_linear, clf, title='Linear Perceptron')
+# ax1 = plt.subplot(121)
+# utils.plot_classifier(ax1, X, y_linear, clf, title='Linear Perceptron')
 
 
 
-clf = svm.SVC(kernel='linear')
-clf.fit(X, y_linear)
+# clf = svm.SVC(kernel='linear')
+# clf.fit(X, y_linear)
 
-ax2 = plt.subplot(122)
-utils.plot_classifier(ax2, X, y_linear, clf, title='Linear SVM')
+# ax2 = plt.subplot(122)
+# utils.plot_classifier(ax2, X, y_linear, clf, title='Linear SVM')
 
-plt.show()
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 3 - Quadratic Models
@@ -65,36 +65,36 @@ plt.show()
 # Part 3.1 - Quadratic Data Transformation
 #-------------------------------------------------------------------------------
 
-def phi_quad(x):
-    raise NotImplementedError
+# def phi_quad(x):
+#     raise NotImplementedError
 
-X_quad = np.array([phi_quad(x) for x in X])
+# X_quad = np.array([phi_quad(x) for x in X])
 
-clf = svm.SVC(kernel='linear')
-clf.fit(X_quad, y_quad)
+# clf = svm.SVC(kernel='linear')
+# clf.fit(X_quad, y_quad)
 
 
-ax1 = plt.subplot(221)
-utils.plot_data(ax1, X, y_quad, title='Original data')
-ax3 = plt.subplot(223)
-utils.plot_classifier(ax3, X_quad, y_quad, clf, title='Linear classifier on transformed data')
-ax4 = plt.subplot(224)
-utils.plot_classifier(ax4, X, y_quad, clf, phi=phi_quad, title='Projection of classifier onto original data')
-plt.show()
+# ax1 = plt.subplot(221)
+# utils.plot_data(ax1, X, y_quad, title='Original data')
+# ax3 = plt.subplot(223)
+# utils.plot_classifier(ax3, X_quad, y_quad, clf, title='Linear classifier on transformed data')
+# ax4 = plt.subplot(224)
+# utils.plot_classifier(ax4, X, y_quad, clf, phi=phi_quad, title='Projection of classifier onto original data')
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 3.2 - Quadratic Kernel
 #-------------------------------------------------------------------------------
 
-def quad_kernel(x_j, x_i):
-    raise NotImplementedError
+# def quad_kernel(x_j, x_i):
+#     raise NotImplementedError
 
-clf = lab4.KernelPerceptron(kernel=quad_kernel)
-clf.fit(X, y_quad)
+# clf = lab4.KernelPerceptron(kernel=quad_kernel)
+# clf.fit(X, y_quad)
 
-ax = plt.gca()
-utils.plot_classifier(ax, X, y_quad, clf, title='Quadratic Perceptron')
-plt.show()
+# ax = plt.gca()
+# utils.plot_classifier(ax, X, y_quad, clf, title='Quadratic Perceptron')
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 4 - Radial Models
@@ -104,81 +104,81 @@ plt.show()
 # Part 4.1 - Radial Data Transformation
 #-------------------------------------------------------------------------------
 
-def phi_radial(x):
-    raise NotImplementedError
+# def phi_radial(x):
+#     raise NotImplementedError
 
-X_radial = np.array([phi_radial(x) for x in X])
+# X_radial = np.array([phi_radial(x) for x in X])
 
-clf = svm.SVC(kernel='linear')
-clf.fit(X_radial, y_radial)
+# clf = svm.SVC(kernel='linear')
+# clf.fit(X_radial, y_radial)
 
-ax1 = plt.subplot(221)
-utils.plot_data(ax1, X, y_radial, title='Original data')
-ax3 = plt.subplot(223)
-utils.plot_classifier(ax3, X_radial, y_radial, clf, title='Linear classifier on transformed data')
-ax4 = plt.subplot(224)
-utils.plot_classifier(ax4, X, y_radial, clf, phi=phi_radial, title='Projection of classifier onto original data')
-plt.show()
+# ax1 = plt.subplot(221)
+# utils.plot_data(ax1, X, y_radial, title='Original data')
+# ax3 = plt.subplot(223)
+# utils.plot_classifier(ax3, X_radial, y_radial, clf, title='Linear classifier on transformed data')
+# ax4 = plt.subplot(224)
+# utils.plot_classifier(ax4, X, y_radial, clf, phi=phi_radial, title='Projection of classifier onto original data')
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 4.2 - Radial Kernel
 #-------------------------------------------------------------------------------
 
-def radial_kernel(x_j, x_i):
-    raise NotImplementedError
+# def radial_kernel(x_j, x_i):
+#     raise NotImplementedError
 
-clf = lab4.KernelPerceptron(kernel=radial_kernel)
-clf.fit(X, y_radial)
+# clf = lab4.KernelPerceptron(kernel=radial_kernel)
+# clf.fit(X, y_radial)
 
-ax1 = plt.subplot(121)
-utils.plot_classifier(ax1, X, y_radial, clf, title='RBF Perceptron')
+# ax1 = plt.subplot(121)
+# utils.plot_classifier(ax1, X, y_radial, clf, title='RBF Perceptron')
 
 
-clf = svm.SVC(kernel='rbf')
-clf.fit(X, y_radial)
+# clf = svm.SVC(kernel='rbf')
+# clf.fit(X, y_radial)
 
-ax2 = plt.subplot(122)
-utils.plot_classifier(ax2, X, y_radial, clf, title='RBF SVM')
+# ax2 = plt.subplot(122)
+# utils.plot_classifier(ax2, X, y_radial, clf, title='RBF SVM')
 
-plt.show()
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 5 - Angle Models
 #-------------------------------------------------------------------------------
 
-def phi_angular(x):
-    raise NotImplementedError
+# def phi_angular(x):
+#     raise NotImplementedError
 
-X_angular = np.array([phi_angular(x) for x in X])
+# X_angular = np.array([phi_angular(x) for x in X])
 
-clf = svm.SVC(kernel='linear')
-clf.fit(X_angular, y_angular)
+# clf = svm.SVC(kernel='linear')
+# clf.fit(X_angular, y_angular)
 
-ax1 = plt.subplot(221)
-utils.plot_data(ax1, X, y_angular, title='Original data')
-ax3 = plt.subplot(223)
-utils.plot_classifier(ax3, X_angular, y_angular, clf, title='Linear classifier on transformed data')
-ax4 = plt.subplot(224)
-utils.plot_classifier(ax4, X, y_angular, clf, phi=phi_angular, title='Projection of classifier onto original data')
-plt.show()
+# ax1 = plt.subplot(221)
+# utils.plot_data(ax1, X, y_angular, title='Original data')
+# ax3 = plt.subplot(223)
+# utils.plot_classifier(ax3, X_angular, y_angular, clf, title='Linear classifier on transformed data')
+# ax4 = plt.subplot(224)
+# utils.plot_classifier(ax4, X, y_angular, clf, phi=phi_angular, title='Projection of classifier onto original data')
+# plt.show()
 
 #-------------------------------------------------------------------------------
 # Part 6 - Bonus: k-Nearest Neighbors
 #-------------------------------------------------------------------------------
 
-# Generate and plot the clustered data
-X_cluster, y_cluster = make_blobs(n_samples=200, centers=[[0,4], [-4,0], [4,0], [0,-4], [0,0]])
+# # Generate and plot the clustered data
+# X_cluster, y_cluster = make_blobs(n_samples=200, centers=[[0,4], [-4,0], [4,0], [0,-4], [0,0]])
 
-ax1 = plt.subplot(331)
-utils.plot_data(ax1, X_cluster, y_cluster, title='Clusters')
+# ax1 = plt.subplot(331)
+# utils.plot_data(ax1, X_cluster, y_cluster, title='Clusters')
 
-# Plot KNN predictions for different values of k
-for i in trange(8):
-    k = i + 1
-    clf = KNeighborsClassifier(n_neighbors=k)
-    clf.fit(X_cluster, y_cluster)
+# # Plot KNN predictions for different values of k
+# for i in trange(8):
+#     k = i + 1
+#     clf = KNeighborsClassifier(n_neighbors=k)
+#     clf.fit(X_cluster, y_cluster)
 
-    axi = plt.subplot(330 + i + 2)
-    utils.plot_classifier(axi, X_cluster, y_cluster, clf, title='{}-Nearest Neighbors'.format(k))
+#     axi = plt.subplot(330 + i + 2)
+#     utils.plot_classifier(axi, X_cluster, y_cluster, clf, title='{}-Nearest Neighbors'.format(k))
 
-plt.show()
+# plt.show()
